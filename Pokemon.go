a6720a83715f04f34c4bcc65d149fcbb516e43d8
Jason Semken <jason.semken@gmail.com>
2024-02-28 20:35:00 +1000

    Include pokemon stats

--- Pokemon.go
+++ Pokemon.go
@@ -8,12 +8,29 @@ import (
 	"net/http"
 	"os"
 	"strings"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 )
+
+var responseObject Response
 
 // A Response struct to map the Entire Response
 type Response struct {
-	Name      string `json:"name"`
-	PokemonID int    `json:"id"`
+	Name        string      `json:"name"`
+	PokemonID   int         `json:"id"`
+	PokemonStat []PokeStats `json:"stats"`
+}
+
+// Struct to hold pokemon stat and value
+type PokeStats struct {
+	StatName Stat `json:"stat"`
+	Value    int  `json:"base_stat"`
+}
+
+// Collects the name of the Pokemon stat
+type Stat struct {
+	Name string `json:"name"`
 }
 
 // Appends the users input onto the api string
@@ -36,6 +53,23 @@ func userInput() string {
 	return fI
 }
 
+// Compares the pokemon ID to 0, 0 means the user input does not make a working API string. Returns requested Pokemon if not 0
+func requestReturn() {
+
+	if responseObject.PokemonID == 0 {
+		fmt.Println("\nThat's Not a Pokemon.")
+	} else {
+		fmt.Printf("\nName: %v\n", cases.Title(language.Und, cases.NoLower).String(responseObject.Name))
+		fmt.Printf("Pokedex ID: %v\n", responseObject.PokemonID)
+		fmt.Printf("HP: %v\n", responseObject.PokemonStat[0].Value)
+		fmt.Printf("Attack: %v\n", responseObject.PokemonStat[1].Value)
+		fmt.Printf("Defense: %v\n", responseObject.PokemonStat[2].Value)
+		fmt.Printf("Speed: %v\n", responseObject.PokemonStat[5].Value)
+		fmt.Printf("Special Attack: %v\n", responseObject.PokemonStat[3].Value)
+		fmt.Printf("Special Defense: %v\n", responseObject.PokemonStat[4].Value)
+	}
+}
+
 func main() {
 
 	response, err := http.Get(apiAddress())
@@ -49,15 +83,8 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var responseObject Response
 	json.Unmarshal(responseData, &responseObject)
 
-	// Compares the pokemon ID to 0, 0 means the user input does not make a working API string
-	if responseObject.PokemonID == 0 {
-		fmt.Println("\nThat's Not a Pokemon.")
-	} else {
-		fmt.Println("\nName:" + responseObject.Name)
-		fmt.Printf("Pokedex ID: %v\n", responseObject.PokemonID)
-	}
+	requestReturn()
 
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module pokedex
 
 go 1.21.3
+
+require golang.org/x/text v0.14.0
